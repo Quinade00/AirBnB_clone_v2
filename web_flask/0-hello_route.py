7879ec26c24a875c7fa5b20690c0037081e903c5
Quinade00 <olaleyeadeola27@yahoo.com>
2024-04-21 04:13:22 -0700

    I just added new files

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
-        """returns Hello HBNB!"""
+def my_function():
+    if some_condition :
             return 'Hello HBNB!'
 
         if __name__ == '__main__':
